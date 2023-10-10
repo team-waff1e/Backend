3fb0eefe83fb2bf88f157c7c0631c5deb74c7694
JehunYoo <percyu98@gmail.com>
2023-10-10 15:36:07 +0900

    Initial commit

---
+++ src/main/java/io/github/teamwaff1e/waffle/WaffleBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.teamwaff1e.waffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaffleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/teamwaff1e/waffle/WaffleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.teamwaff1e.waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
