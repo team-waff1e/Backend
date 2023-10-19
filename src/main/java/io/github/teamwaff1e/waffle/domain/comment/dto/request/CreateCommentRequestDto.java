e18d858ae5ba58d746f38623d2e888a3f936993f
JehunYoo <percyu98@gmail.com>
2023-10-19 15:31:11 +0900

    feat: Add waffleId and commentId validation

--- src/main/java/io/github/teamwaff1e/waffle/domain/comment/dto/request/CreateCommentRequestDto.java
+++ src/main/java/io/github/teamwaff1e/waffle/domain/comment/dto/request/CreateCommentRequestDto.java
@@ -2,6 +2,7 @@ package io.github.teamwaff1e.waffle.domain.comment.dto.request;
 
 import io.github.teamwaff1e.waffle.domain.dto.RequestDto;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CreateCommentRequestDto implements RequestDto {
 
+    @NotNull
     private Long waffleId;
 
     @NotBlank
