7486db654752a3efbf8b22aa674253c626ca1056
Jehun Yoo <percyu98@gmail.com>
2023-10-27 15:11:26 +0900

    feat: block unauthenticated user if authentication required

--- src/main/java/io/github/teamwaff1e/waffle/global/exception/code/ErrorCode.java
+++ src/main/java/io/github/teamwaff1e/waffle/global/exception/code/ErrorCode.java
@@ -8,11 +8,14 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     LOGIN_FAILURE(1111), // todo
+
     LOGOUT_ILLEGAL_STATE(1112), // todo
 
     EMAIL_ALREADY_EXISTS(1121),
 
-    NICKNAME_ALREADY_EXISTS(1122);
+    NICKNAME_ALREADY_EXISTS(1122),
+
+    UNAUTHENTICATED(1115);
 
 
     private final int code;
---
+++ src/main/java/io/github/teamwaff1e/waffle/global/exception/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package io.github.teamwaff1e.waffle.global.exception.controller;
+
+import io.github.teamwaff1e.waffle.global.exception.auth.WAuthenticationException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/errors")
+public class ErrorController {
+
+    @RequestMapping(value = "/unauthenticated")
+    public void unauthenticatedRequest(HttpServletRequest request, HttpServletResponse response) {
+        throw new WAuthenticationException();
+    }
+}
