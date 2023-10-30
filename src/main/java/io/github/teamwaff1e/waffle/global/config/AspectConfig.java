ba112c12ff8ca6ed2e6404a8195601450fa44ef2
Jehun Yoo <percyu98@gmail.com>
2023-10-30 16:18:07 +0900

    feat: add method tracer TraceAspect

---
+++ src/main/java/io/github/teamwaff1e/waffle/global/config/AspectConfig.java
@@ -0,0 +1,14 @@
+package io.github.teamwaff1e.waffle.global.config;
+
+import io.github.teamwaff1e.waffle.global.aspect.TraceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AspectConfig {
+
+    @Bean
+    public TraceAspect traceAspect() {
+        return new TraceAspect();
+    }
+}
