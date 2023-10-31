5d31e585c56fd217ae7da26d7888b710480ed2f1
dongkun yoo <73277351+peppermintt0504@users.noreply.github.com>
2023-10-31 16:06:48 +0900

    feat(follow): add Validated

--- src/main/java/io/github/teamwaff1e/waffle/domain/member/controller/MemberController.java
+++ src/main/java/io/github/teamwaff1e/waffle/domain/member/controller/MemberController.java
@@ -6,10 +6,20 @@ import io.github.teamwaff1e.waffle.domain.member.dto.request.FollowRequestDto;
 import io.github.teamwaff1e.waffle.domain.member.dto.request.UpdateMemberRequestDto;
 import io.github.teamwaff1e.waffle.domain.member.dto.response.MemberResponseDto;
 import io.github.teamwaff1e.waffle.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,25 +30,39 @@ public class MemberController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MemberResponseDto createMember(@ModelAttribute CreateMemberRequestDto memberRequestDto, BindingResult bindingResult) {
-        return memberService.createMember(memberRequestDto);
+    public ResponseEntity createMember(@Validated @ModelAttribute CreateMemberRequestDto memberRequestDto,
+                                          BindingResult bindingResult) {
+
+
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors().stream().map(v->{
+                Map<String,String> map = new HashMap<>();
+                map.put("errorCode",v.getCode());
+                map.put("message",v.getDefaultMessage());
+                return map;
+            }));
+        }
+        System.out.println(22222);
+        return ResponseEntity.ok(memberService.createMember(memberRequestDto));
+//        return memberService.createMember(memberRequestDto);
     }
 
     @GetMapping("/{memberId}")
     @ResponseStatus(HttpStatus.OK)
     public MemberResponseDto readMemberById(@PathVariable Long memberId) {
+        System.out.println("!@#DAS");
         return memberService.readMember(memberId);
     }
 
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     public MemberResponseDto readMember() {
-        return memberService.readMember(1L); //Todo : 임시 Id 변경해야 함
+        return memberService.readMember(5L); //Todo : 임시 Id 변경해야 함
     }
 
     @PatchMapping()
     @ResponseStatus(HttpStatus.OK)
-    public MemberResponseDto updateMember(@ModelAttribute UpdateMemberRequestDto memberRequestDto) {
+    public MemberResponseDto updateMember(@Validated @ModelAttribute UpdateMemberRequestDto memberRequestDto) {
         return memberService.updateMember(1L,memberRequestDto); //Todo : 임시 Id 변경해야 함
     }
 
@@ -50,15 +74,15 @@ public class MemberController {
 
     @PostMapping("/follow")
     @ResponseStatus(HttpStatus.OK)
-    public void follow (@ModelAttribute FollowRequestDto followRequestDto){
+    public void follow (@Validated @ModelAttribute FollowRequestDto followRequestDto){
     }
     @PostMapping("/unfollow")
     @ResponseStatus(HttpStatus.OK)
-    public void unfollow (@ModelAttribute FollowRequestDto followRequestDto){
+    public void unfollow (@Validated @ModelAttribute FollowRequestDto followRequestDto){
     }
     @GetMapping("/follow/{memberId}")
     @ResponseStatus(HttpStatus.OK)
-    public void readFollowById (@PathVariable Long memberd){
+    public void readFollowById (@NotEmpty @Positive @PathVariable Long memberd){
 
     }
 }
--- src/main/java/io/github/teamwaff1e/waffle/domain/member/dto/request/CreateMemberRequestDto.java
+++ src/main/java/io/github/teamwaff1e/waffle/domain/member/dto/request/CreateMemberRequestDto.java
@@ -1,6 +1,7 @@
 package io.github.teamwaff1e.waffle.domain.member.dto.request;
 
 import io.github.teamwaff1e.waffle.global.dto.request.RequestDto;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,9 +9,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class CreateMemberRequestDto implements RequestDto {
-    @NotBlank
+
+    @NotBlank(message = "이메일 입력이 없습니다.")
+    @Email( message = "이메일 형식이 맞지 않습니다.")
     private String email;
+    @NotBlank(message = "이름 입력이 없습니다.")
     private String name;
+    @NotBlank(message = "비밀번호 입력이 없습니다.")
     private String password;
+    @NotBlank(message = "닉네임 입력이 없습니다.")
     private String nickname;
 }
