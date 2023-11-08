010fa24b90e4c96ea4e96eadeec21414a39edbbd
Jehun Yoo <percyu98@gmail.com>
2023-11-08 13:20:02 +0900

    refactor: facilitate GET /waffles/{waffleId}/comments/{commentId}

--- src/main/java/io/github/teamwaff1e/waffle/domain/comment/dto/converter/CommentToResponseConverter.java
+++ src/main/java/io/github/teamwaff1e/waffle/domain/comment/dto/converter/CommentToResponseConverter.java
@@ -1,19 +1,30 @@
 package io.github.teamwaff1e.waffle.domain.comment.dto.converter;
 
 import io.github.teamwaff1e.waffle.domain.comment.dto.response.CommentResponseDto;
+import io.github.teamwaff1e.waffle.domain.member.dto.response.OwnerResponseDto;
 import io.github.teamwaff1e.waffle.domain.comment.entity.Comment;
+import io.github.teamwaff1e.waffle.domain.member.entity.Member;
 import io.github.teamwaff1e.waffle.global.dto.converter.DtoConverter;
 
 public class CommentToResponseConverter implements DtoConverter<Comment, CommentResponseDto> {
 
     @Override
     public CommentResponseDto convert(Comment comment) {
+        Member member = comment.getMember();
+
+        OwnerResponseDto ownerResponseDto = OwnerResponseDto.builder()
+                .id(member.getId())
+                .nickname(member.getNickname())
+                .profile_url(member.getProfileUrl())
+                .build();
+
         return CommentResponseDto.builder()
                 .id(comment.getId())
                 .waffleId((comment.getWaffle().getId()))
                 .content(comment.getContent())
                 .createdAt(comment.getCreatedAt())
                 .updatedAt(comment.getUpdatedAt())
+                .ownerResponseDto(ownerResponseDto)
                 .build();
     }
 }
