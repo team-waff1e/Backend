9b8e0d6ee9e25585c92eda168aaee7eab797b11b
Jehun Yoo <percyu98@gmail.com>
2023-11-09 14:29:12 +0900

    feat: handle MethodArgumentNotValidException and respond error messages

--- src/main/java/io/github/teamwaff1e/waffle/global/dto/response/ExceptionResponseDto.java
+++ src/main/java/io/github/teamwaff1e/waffle/global/dto/response/ExceptionResponseDto.java
@@ -1,14 +1,24 @@
 package io.github.teamwaff1e.waffle.global.dto.response;
 
 import io.github.teamwaff1e.waffle.global.error.code.ErrorCode;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class ExceptionResponseDto implements ResponseDto {
 
     private final int errorCode;
+    private final String errorMessage;
+    private final List<String> errorDetails;
 
     public ExceptionResponseDto(ErrorCode errorCode) {
+        this(errorCode, new ArrayList<>()); // todo: null or empty list?
+    }
+
+    public ExceptionResponseDto(ErrorCode errorCode, List<String> details) {
         this.errorCode = errorCode.getCode();
+        this.errorMessage = errorCode.getMessage();
+        this.errorDetails = details;
     }
 }
